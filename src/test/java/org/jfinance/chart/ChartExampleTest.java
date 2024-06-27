8de2e07414f1ea558e334acf5cd0a344231fb146
hitzseb <hitzseb@gmail.com>
2024-06-27 14:20:06 -0300

    chart example

--- src/test/java/org/jfinance/chart/ChartExampleTest.java
+++ src/test/java/org/jfinance/chart/ChartExampleTest.java
@@ -1,12 +1,11 @@
-package org.jfinance;
+package org.jfinance.chart;
 
 import org.jfinance.chart.model.Chart;
 import org.jfinance.chart.service.ChartService;
-import org.jfinance.chart.utils.TimestampConverter;
 
 import java.io.IOException;
 
-public class Main {
+public class ChartExampleTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         Chart rangeChart = ChartService.getChart("AAPL", "1d", "5d");
         Chart periodChart = ChartService.getChart("AAPL", "1d", "2024-03-01", "2024-04-01");
@@ -17,7 +16,7 @@ public class Main {
 
         System.out.println("\n--------------------------------------------------------------------------------------");
 
-        System.out.println("\n" +periodChart.toString() + "\n");
+        System.out.println("\n" + periodChart.toString() + "\n");
         System.out.println("BS timezone" + "\n");
         periodChart.printTable("America/Argentina/Buenos_Aires");
     }
